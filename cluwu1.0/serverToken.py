3ada47d56c4cba07d8f343cf0f860bf9cbfeff75
root <root@vps.server.local>
2020-05-21 07:49:10 +0000

    new class

---
+++ cluwu1.0/serverToken.py
@@ -0,0 +1,38 @@
+import random
+from serverPlayer import *
+
+#characterChoices = [("Ms Scarlet", 16), ("Mrs White", 191), ("Colonel Mustard", 590), ("Mr Green", 585), ("Professor Plum", 432), ("Ms Peacock", 120)]
+characterChoices = [("Ms Scarlet", 0, 16), ("Mrs White", 7, 23), ("Colonel Mustard", 24, 14), ("Mr Green", 24, 9), ("Professor Plum", 18, 0), ("Ms Peacock", 5, 0)]
+
+class ServerToken:
+    def __init__(self, characterName, tokenLocationX, tokenLocationY):
+        self.tokenCharacter = characterName
+        self.tokenXLoc = tokenLocationX
+        self.tokenYLoc = tokenLocationY
+
+    def getTokenCharacter(self):
+        return self.tokenCharacter
+
+    def getTokenXLoc(self):
+        return self.tokenXLoc
+
+    def getTokenYLoc(self):
+        return self.tokenYLoc
+
+
+
+def assignTokens(playerList):
+    print("did we make it in assign?")
+    tokenList = []
+    for characters in characterChoices:
+        print("in characters")
+        objectName = characters[0]
+        objectName = ServerToken(characters[0], characters[1], characters[2])
+        tokenList.append(objectName)
+    print("after the first for loop")
+    for x in range(len(playerList)):
+        print("is blain right?")
+        playerList[x].setMyToken(tokenList[x])
+        print(playerList[x].getMyToken())
+    print("after the second for loop")
+
