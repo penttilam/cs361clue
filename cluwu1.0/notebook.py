0fec8abad768f5301a41a5cb29f3425eaa5ff9fc
Morgan Rose <58056433+ShochuRose@users.noreply.github.com>
2020-05-23 22:13:07 -0700

    Update notebook.py
    now the buttons are kawaii and sugoi desu neeeeee~~~~~~~

--- cluwu1.0/notebook.py
+++ cluwu1.0/notebook.py
@@ -5,8 +5,11 @@ def createNotebook(notebook):
     d = 50
     width = notebook.getWidth()
     height = notebook.getHeight()
-    xPos = [((width*2)/12),((width*5)/12),((width*8)/12)]
-    yPos = [((height*9)/d),((height*13)/d),((height*20)/d),((height*24)/d),((height*28)/d),((height*35)/d),((height*39)/d)]
+    ##xPos = [((width*2)/12),((width*5)/12),((width*8)/12)]
+    ##yPos = [((height*9)/d),((height*13)/d),((height*20)/d),((height*24)/d),((height*28)/d),((height*35)/d),((height*39)/d)]
+
+    xPos = [58, 170, 280]
+    yPos = [125, 150, 250, 275, 300, 403, 427]
     
     for x in xPos:
             for y in yPos: 
